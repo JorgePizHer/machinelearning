3cb82983bfb49d353bc1afcec791e8de94def888
JorgePizHer <jpizarroh166@alumnos.imf.com>
2024-02-10 16:36:11 +0100

    procsar imagenes e identificar elementos

---
+++ codigo/guardajson2.php
@@ -0,0 +1,22 @@
+<?php
+    
+    $archivo = "datos/prueba.json";
+
+    $datos = [
+        'elemento' => $_GET['patron'],
+        'datos' => $_GET['datos']
+    ];
+    
+    
+
+    $datosexistentes = file_get_contents($archivo);
+    $datosexistentesjson = json_decode($datosexistentes);
+    
+    array_push($datosexistentesjson,$datos);
+    
+    $json = json_encode($datosexistentesjson,JSON_PRETTY_PRINT);
+
+    file_put_contents($archivo,$json);
+
+
+?>
